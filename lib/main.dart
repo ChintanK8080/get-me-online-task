3fa8e06159b10390d87cf0d0dd4da36d2d10b653
chintank8080 <chintank8080@gmail.com>
2024-05-24 15:04:04 +0530

    disabled device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:getme_online_task/views/screens/home_screen.dart';
 
@@ -10,7 +9,8 @@ main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(DevicePreview(
-    enabled: !kReleaseMode,
+    enabled: false,
+    // !kReleaseMode,
     builder: (context) => const MyApp(),
   ));
 }
